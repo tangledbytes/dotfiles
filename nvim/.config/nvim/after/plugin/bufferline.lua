4ef1d59d1c0f0d4f09b3baea5abd4315e4f11b63
Utkarsh Srivastava <srivastavautkarsh8097@gmail.com>
2022-12-25 15:24:39 +0530

    add sidebar awareness
    Signed-off-by: Utkarsh Srivastava <srivastavautkarsh8097@gmail.com>

--- nvim/.config/nvim/after/plugin/bufferline.lua
+++ nvim/.config/nvim/after/plugin/bufferline.lua
@@ -2,5 +2,6 @@ require("bufferline").setup({
     options = {
         diagnostics = 'nvim_lsp',
         mode = 'tabs',
+        offsets = {{filetype = "NvimTree", text = "File Explorer", text_align = "center"}},
     }
 })
