4a06276804b13cc0b003164e3c3d8bb0b4b0e924
Utkarsh Srivastava <srivastavautkarsh8097@gmail.com>
2022-12-26 05:33:32 +0530

    remove vscode theme
    Signed-off-by: Utkarsh Srivastava <srivastavautkarsh8097@gmail.com>

--- nvim/.config/nvim/plugin/packer_compiled.lua
+++ nvim/.config/nvim/plugin/packer_compiled.lua
@@ -248,11 +248,6 @@ _G.packer_plugins = {
     loaded = true,
     path = "/Users/usrivast/.local/share/nvim/site/pack/packer/start/vim-sleuth",
     url = "https://github.com/tpope/vim-sleuth"
-  },
-  ["vscode.nvim"] = {
-    loaded = true,
-    path = "/Users/usrivast/.local/share/nvim/site/pack/packer/start/vscode.nvim",
-    url = "https://github.com/Mofiqul/vscode.nvim"
   }
 }
 
