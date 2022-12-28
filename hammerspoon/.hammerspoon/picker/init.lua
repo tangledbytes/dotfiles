917568c31a154dcb3e9e3191aa73de4d52c94393
Utkarsh Srivastava <srivastavautkarsh8097@gmail.com>
2022-12-28 21:00:49 +0530

    handle cases of no selection
    Signed-off-by: Utkarsh Srivastava <srivastavautkarsh8097@gmail.com>

--- hammerspoon/.hammerspoon/picker/init.lua
+++ hammerspoon/.hammerspoon/picker/init.lua
@@ -3,16 +3,21 @@ local fs = require("picker.fs")
 local scripts = require("picker.scripts")
 
 pickers.register_picker("home_search", function(tbl)
+  if tbl == nil then return end
   scripts.osascript_open_directory_in_iterm(tbl["text"])
 end, "Search in $HOME")
+
 pickers.picker_register_query_changed_callback(
 	"home_search",
 	fs.get_fs_debounced(pickers.choice_fn_for_picker("home_search"), "~", "d", "4")
 )
 
+
 pickers.register_picker("dev_search", function(tbl)
+  if tbl == nil then return end
   scripts.osascript_open_directory_in_iterm(tbl["text"])
 end, "Search in ~/dev")
+
 pickers.picker_register_query_changed_callback(
 	"dev_search",
 	fs.get_fs_debounced(pickers.choice_fn_for_picker("dev_search"), "$HOME/dev", "d", "3")
