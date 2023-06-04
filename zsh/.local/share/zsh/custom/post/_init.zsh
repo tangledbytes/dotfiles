e271dc2231f939ea3d4c1ee3b6b2b3456c90420e
Utkarsh Srivastava <srivastavautkarsh8097@gmail.com>
2023-06-04 16:21:14 +0530

    cleanup zsh config
    Signed-off-by: Utkarsh Srivastava <srivastavautkarsh8097@gmail.com>

---
+++ zsh/.local/share/zsh/custom/post/_init.zsh
@@ -0,0 +1,26 @@
+# SETUP TOOL DEPENDENT ENVIRONMENT VARIABLES
+
+# No need to setup anything for Rust because rustup does it for us in .zshenv file
+
+# For Golang
+if command -v go &>/dev/null
+then
+  export GOPATH=$(go env GOPATH)
+  export GOROOT=$(go env GOROOT)
+  path+=("$GOPATH/bin")
+  path+=("$GOROOT/bin")
+fi
+
+# For Zig
+# if command -v zig &>/dev/null
+# then
+# fi
+
+# For Node (NVM)
+if [ -f "$HOME/.nvm/nvm.sh" ]
+then
+  export NVM_DIR="$HOME/.nvm"
+  source "$NVM_DIR/nvm.sh"
+fi
+
+export PATH
--- zsh/.local/share/zsh/custom/pre/setup.zsh
+++ zsh/.local/share/zsh/custom/pre/setup.zsh
@@ -1,20 +1,13 @@
-# SETUP ENVIRONMENT VARIABLES 
-path+=("$(go env GOPATH)/bin")
+# SETUP ENVIRONMENT VARIABLES ================================================================
 path+=("$HOME/bin")
 path+=("$HOME/.local/bin")
 export PATH
-
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 
 export GPG_TTY=$(tty)
 export EDITOR=nvim
 export CLICOLOR=1 # Force color is ls output
 
-export GOPATH=$(go env GOPATH)
-export GOROOT=$(go env GOROOT)
-
-# SETUP ZSH OPTIONS
+# SETUP ZSH OPTIONS ==========================================================================
 setopt autocd
 ## ZSH HISTORY OPTIONS
 HISTFILE=~/.zsh_history
@@ -22,7 +15,7 @@ HISTSIZE=999999999
 SAVEHIST=$HISTSIZE
 setopt appendhistory
 
-# SETUP FPATH AUTOCOMPLETE
+# SETUP FPATH AUTOCOMPLETE ==================================================================
 if type brew &>/dev/null # Do it for brew here -- linux can be handled seperately
 then
   FPATH="$(brew --prefix)/share/zsh/site-functions:${FPATH}"
@@ -31,6 +24,7 @@ fi
 autoload -Uz compinit # Load compinit as a function
 compinit
 
+# SETUP ZSH PLUGINS ==========================================================================
 # NOTE: Setup for history substring search with vim mode -- won't work if the plugins are not
 # installed beforehand
 bindkey -M vicmd 'k' history-substring-search-up
