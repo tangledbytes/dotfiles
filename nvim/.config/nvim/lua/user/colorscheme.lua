404953b2bb0be22a67ccf468eb750229b5b6aec4
Utkarsh Srivastava <srivastavautkarsh8097@gmail.com>
2023-07-29 06:46:57 +0000

    add transparency to neovim and tmux
    Signed-off-by: Utkarsh Srivastava <srivastavautkarsh8097@gmail.com>

--- nvim/.config/nvim/lua/user/colorscheme.lua
+++ nvim/.config/nvim/lua/user/colorscheme.lua
@@ -1,5 +1,11 @@
 local colorscheme = "carbonfox"
 
+require('nightfox').setup({
+  options = {
+    transparent = true,
+  }
+})
+
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
   vim.notify("colorscheme " .. colorscheme .. " not found!")
