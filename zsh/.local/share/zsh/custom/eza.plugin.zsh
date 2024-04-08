90a6dd2ac3e99486732e1c6217b4081416677b03
Utkarsh Srivastava <srivastavautkarsh8097@gmail.com>
2024-04-08 19:54:51 +0530

    eza aliases

---
+++ zsh/.local/share/zsh/custom/eza.plugin.zsh
@@ -0,0 +1,9 @@
+alias eza='eza --color=always --icons=always'
+alias e='eza -lh'
+alias ea='eza -lAh'
+alias er='eza -lRf'
+alias el='eza -l'
+alias eE='eza -lhHbai'
+alias eEe='eza -lhHbai@'
+alias edot='eza -ld .*'
+
