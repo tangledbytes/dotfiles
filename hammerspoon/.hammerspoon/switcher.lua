62eb8d959e0df3433be704fe838e5d9aa2c6deb5
Utkarsh Srivastava <srivastavautkarsh8097@gmail.com>
2024-04-13 18:54:45 +0530

    hammerspoon - remove a lot of unused stuff
    Signed-off-by: Utkarsh Srivastava <srivastavautkarsh8097@gmail.com>

--- hammerspoon/.hammerspoon/switcher.lua
+++
@@ -1,9 +0,0 @@
-all_window_switcher = hs.window.switcher.new(hs.window.filter.new():setCurrentSpace():setDefaultFilter{})
-
--- Bind alt+tab and alt+shift+tab to switch between window
-hs.hotkey.bind({"alt"}, "tab", function()
-  all_window_switcher:next()
-end)
-hs.hotkey.bind({"alt", "shift"}, "tab", function()
-  all_window_switcher:previous()
-end)
