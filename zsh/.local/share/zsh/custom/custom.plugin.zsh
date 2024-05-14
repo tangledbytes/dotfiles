9e01f0c90f37041d4dd363b13c66f646b52caacd
Utkarsh Srivastava <srivastavautkarsh8097@gmail.com>
2024-05-14 17:38:17 +0530

    add nice utility functions
    Signed-off-by: Utkarsh Srivastava <srivastavautkarsh8097@gmail.com>

--- zsh/.local/share/zsh/custom/custom.plugin.zsh
+++ zsh/.local/share/zsh/custom/custom.plugin.zsh
@@ -31,6 +31,41 @@ function ucls () {
   printf '\033[2J\033[3J\033[1;1H'
 }
 
+function vmc() {
+	local target_path="$1"
+	local linux_home="/home/$USER.linux"
+	local full_path="$(cd "$target_path" && pwd)"
+	local linux_path="${linux_home}${full_path/#$HOME}"
+
+	echo "Opening $linux_path in VSCode on Linux"
+	code --remote ssh-remote+localhost "$linux_path"
+}
+
+function rndc() {
+	local target_path="$1"
+	local rnd_home="/home/utkarsh"
+	local rnd_path="$target_path"
+
+	if [ -d "$target_path" ]; then
+		local full_path="$(cd "$target_path" && pwd)"
+		rnd_path="${rnd_home}${full_path/#$HOME}"
+	fi
+
+	echo "Opening $rnd_path in VSCode in RnD"
+	code --remote ssh-remote+rnd "$rnd_path"
+}
+
+function nf() {
+	local opt="$1"
+	if [ -z "$opt" ]; then
+		opt="$(fzf --preview='bat -f {}' --preview-window=right --layout=reverse --height=50%)"
+	fi
+
+	if [ "$?" -eq 0 ]; then
+		nvim "$opt"
+	fi
+}
+
 # SETUP ITERM2 INTEGRATION ===================================================================
 # if [ -e "${HOME}/.iterm2_shell_integration.zsh" ]; then
 #   source "${HOME}/.iterm2_shell_integration.zsh"
--- zsh/.zshrc
+++ zsh/.zshrc
@@ -2,12 +2,3 @@ source ~/.config/zsh/init.zsh
 
 alias vdssh="limactl shell --workdir /home/$USER.linux devenv"
 
-function vmc() {
-	local target_path="$1"
-	local linux_home="/home/$USER.linux"
-	local full_path=$(cd "$target_path" && pwd)
-	local linux_path="${linux_home}${full_path/#$HOME}"
-
-	echo "Opening $linux_path in VSCode on Linux"
-	code --remote ssh-remote+localhost "$linux_path"
-}
