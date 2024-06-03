f6991df08645c0e21213215df763aa84b6c5339e
Utkarsh Srivastava <srivastavautkarsh8097@gmail.com>
2024-06-03 21:58:19 +0530

    fix colorscheme contrast
    Signed-off-by: Utkarsh Srivastava <srivastavautkarsh8097@gmail.com>

--- nvim/.config/nvim/lua/user/colorscheme.lua
+++ nvim/.config/nvim/lua/user/colorscheme.lua
@@ -23,7 +23,7 @@ function M.config()
 		invert_tabline = false,
 		invert_intend_guides = false,
 		inverse = true, -- invert background for search, diffs, statuslines and errors
-		contrast = "", -- can be "hard", "soft" or empty string
+		contrast = "hard", -- can be "hard", "soft" or empty string
 		palette_overrides = {},
 		overrides = {},
 		dim_inactive = false,
