06abd18e55c1958bd44edb1469de057b25fc7a66
Utkarsh Srivastava <srivastavautkarsh8097@gmail.com>
2025-01-19 08:51:54 +0530

    add few more colorschemes
    Signed-off-by: Utkarsh Srivastava <srivastavautkarsh8097@gmail.com>

--- nvim/.config/nvim/lua/user/colorscheme.lua
+++ nvim/.config/nvim/lua/user/colorscheme.lua
@@ -1,10 +1,10 @@
-local M =   {
+local GruvBox =   {
 	"ellisonleao/gruvbox.nvim",
 	lazy = false,
 	priority = 1000,
 }
 
-function M.config()
+function GruvBox.config()
 	require("gruvbox").setup({
 		terminal_colors = true, -- add neovim terminal colors
 		undercurl = true,
@@ -33,4 +33,59 @@ function M.config()
 	vim.cmd.colorscheme('gruvbox')
 end
 
-return M
+local Solarized = {
+	"Tsuzat/NeoSolarized.nvim",
+	lazy = false,
+	priority = 1000,
+}
+
+function Solarized.config()
+	require("NeoSolarized").setup({
+		style = "dark",
+		transparent = true,
+		terminal_colors = true,
+		enable_italics = true,
+		styles = {
+			comments = { italic = true },
+			keywords = { italic = true },
+			functions = { bold = true },
+			variables = {},
+			string = { italic = true },
+			underline = true, -- true/false; for global underline
+			undercurl = true, -- true/false; for global undercurl
+		},
+	})
+
+	vim.cmd.colorscheme('NeoSolarized')
+end
+
+local SolarizedOsaka = {
+	"craftzdog/solarized-osaka.nvim",
+	lazy = false,
+	priority = 1000,
+	opts = {},
+}
+
+function SolarizedOsaka.config()
+	require("solarized-osaka").setup({
+		transparent = true,
+		terminal_colors = true,
+		styles = {
+			comments = { italic = true },
+			keywords = { italic = true },
+			functions = {},
+			variables = {},
+			sidebars = "dark",
+			floats = "dark",
+		},
+		sidebars = { "qf", "help" },
+		day_brightness = 0.3,
+		hide_inactive_statusline = false,
+		dim_inactive = false,
+		lualine_bold = false,
+	})
+
+	vim.cmd.colorscheme('solarized-osaka')
+end
+
+return GruvBox
